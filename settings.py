587a27f2dc7065ee4aa9a44692f8460cc922358e
Yoandre Saavedra <yoandre.saavedra@gmail.com>
2016-07-16 20:14:21 +0100

    Agregado los ficheros estaticos 2

--- settings.py
+++ settings.py
@@ -64,7 +64,7 @@ STATIC_ROOT = 'iaboots/chat/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'iaboots/chat/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
